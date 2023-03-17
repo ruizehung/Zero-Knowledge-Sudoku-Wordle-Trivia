68904b64b51a3601d7ed21922bf21eaa23a06c19
Ray Hung <howard88tw@gmail.com>
2023-03-17 03:29:14 +0000

    Implement backend for aleo sudoku

--- README.md
+++ README.md
@@ -2,3 +2,14 @@
 
 ## Setup
 1. Follow https://developer.aleo.org/testnet/getting_started/deploy_execute to setup a local node in development mode till step 4
+2. Deploy `sudoku.aleo`:
+
+    `cd leo/sudoku && leo build`
+
+    `export PRIVATE_KEY=<PRIVATE_KEY>`
+
+    ```
+    snarkos developer deploy sudoku.aleo --private-key $PRIVATE_KEY --query "http://localhost:3030" --path "leo/sudoku/build/" \
+    --broadcast "http://localhost:3030/testnet3/transaction/broadcast" --fee 600000 \
+    --record "{  owner: aleo14fhcxhsvmsyex23g9v3s6c3m6ep8zc54gv8zc9xtxwugpmqs4czqkx7kkz.private,  gates: 6874999400000u64.private,  _nonce: 3999189760269374026587416243617681089060371078748908085662719605779542084395group.public}"
+    ```
--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,20 @@
+import subprocess
 from typing import List, Union
 
+import requests
 from fastapi import FastAPI
 from pydantic import BaseModel
 
-class Sudoku(BaseModel):
+from aleo import *
+
+class SudokuInput(BaseModel):
+    private_key: str
     puzzle: List[List[int]]
     solution: List[List[int]]
+
+class DecryptRecordInput(BaseModel):
+    view_key: str
+    ciphertext: str
 
 app = FastAPI()
 
@@ -13,14 +22,34 @@ app = FastAPI()
 async def read_root():
     return {"Hello": "World"}
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/leo/transaction/{transaction_id}")
+async def read_transaction(transaction_id: str):
+    try:
+        return requests.get("http://0.0.0.0:3030/testnet3/transaction/at1qpzjqmzddcnzd4fncm3dldh840f2flfvq64f89v06lc3cfdeks9snjwnjz").json()
+    except Exception as e:
+        return {"error": str(e)}
+
+@app.post("/leo/decrypt")
+async def decrypt_record(decrypt_record_input: DecryptRecordInput):
+    return {"output": decrypt_ciphertext(decrypt_record_input.view_key, decrypt_record_input.ciphertext)}
+
+@app.get("/sudoku")
+async def get_new_sudoku():
+    sudoku = generate_valid_sudoku()
+    puzzle = generate_sudoku_puzzle(sudoku)
+    return {"puzzle": puzzle, "solution": sudoku}
 
 @app.post("/leo/sudoku")
-async def solve_sudoku_leo(sudoku: Sudoku):
-    return sudoku
+async def solve_sudoku_leo(sudoku_input: SudokuInput):
+    output = solve_puzzle(sudoku_input.puzzle, sudoku_input.solution, sudoku_input.private_key)
+    if "Successfully broadcast execution" in output:
+        return {"success": True, "output": output}
+    else:
+        return {"success": False, "output": output}
 
 @app.post("/noir/sudoku")
-async def solve_sudoku_noir(sudoku: Sudoku):
-    return sudoku
+async def solve_sudoku_noir(sudoku_input: SudokuInput):
+    return sudoku_input
+
+
+
